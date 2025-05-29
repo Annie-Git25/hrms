ee7f6860595765569bc0bd04b696766b4ed87056
Annie <quenie.abragan@gmail.com>
2025-05-29 14:24:55 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
 // src/App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import EmployeePage from './pages/EmployeePage';
-//import LeavePage from './pages/LeavePage';
-import ScheduleScheduling from './pages/StaffScheduling';
-import TrainingManagement from './pages/TrainingManagement';
-//import ReviewPage from './pages/ReviewPage';
-//import RecognitionPage from './pages/RecognitionPage';
-//import OffboardingPage from './pages/OffboardingPage';
-import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AdminLoginPage from './pages/AdminLoginPage';
+import EmployeeLoginPage from './pages/EmployeeLoginPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        {/* Route paths map to respective modules */}
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/employees" element={<EmployeePage />} />
-        <Route path="/leave" element={<LeavePage />} />
-        <Route path="/schedule" element={<SchedulePage />} />
-        <Route path="/training" element={<TrainingPage />} />
-        <Route path="/reviews" element={<ReviewPage />} />
-        <Route path="/recognition" element={<RecognitionPage />} />
-        <Route path="/offboarding" element={<OffboardingPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin-login" element={<AdminLoginPage />} />
+        <Route path="/employee-login" element={<EmployeeLoginPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
